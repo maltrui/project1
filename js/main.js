5b7cafc9dae0e394dda35cefee33117095a71e3f
maltrui <mcaltrui@gmail.com>
2022-10-14 19:10:26 -0400

    update boardArray after chip move

--- js/main.js
+++ js/main.js
@@ -25,19 +25,29 @@ const movePiecePart1 = function(event){
     if (moveChipPart1 == true){
         moveChipPart1 = false
         moveChipPart2 = true
-        console.log(event.target.parentNode.id.slice(-1))
-        console.log(event.target.parentNode.id.slice(0,1))
-        console.log(event.target.parentNode.id)
+
         console.log(boardArray[parseInt(event.target.parentNode.id.slice(0,1))][parseInt(event.target.parentNode.id.slice(-1))])
-        selectedChipRow = parseInt(event.target.parentNode.id.slice(0,1))
-        selectedChipColumn = parseInt(event.target.parentNode.id.slice(-1))
+        selectedChipRow = parseInt(event.target.parentNode.id.slice(0,1))-1
+        selectedChipColumn = parseInt(event.target.parentNode.id.slice(-1))-1
         selectedChip = event.target
+        console.log(selectedChipRow)
+        console.log(selectedChipColumn)
+        console.log(parseInt(event.target.parentNode.id.slice(-1)))
+
     }
 }
 
 const movePiecePart2 = function(event){
     if (moveChipPart2 == true){
-        boardArray[selectedChipRow][selectedChipColumn] = 0
+        movedToSpaceRow = parseInt(event.target.id.slice(0,1)) - 1
+        movedToSpaceColumn = parseInt(event.target.id.slice(-1)) - 1
+        boardArray[selectedChipRow].splice(selectedChipColumn, 1, 0)
+        if(selectedChip.classList.contains('redChip')){
+            boardArray[movedToSpaceRow].splice(movedToSpaceColumn,1 ,-1)
+        } else {
+            boardArray[movedToSpaceRow].splice(movedToSpaceColumn, 1, 1)
+        }
+        console.log(boardArray)
         event.target.appendChild(selectedChip)
         moveChipPart1 = true
         moveChipPart2 = false
